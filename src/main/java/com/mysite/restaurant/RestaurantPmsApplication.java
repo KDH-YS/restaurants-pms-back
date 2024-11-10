486c3fef45b8e3227c6138370f936549a37b5efb
Jammomu <pridom03@naver.com>
2024-11-10 15:56:02 +0900

    프로젝트 초기화

---
+++ src/main/java/com/mysite/restaurant/RestaurantPmsApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantPmsApplication.class, args);
+	}
+
+}
