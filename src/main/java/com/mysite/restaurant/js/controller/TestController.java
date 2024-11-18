b2e4c3e4d725d7a93d31df82d23ca0aefb0e826a
joo2296 <joo2296@gmail.com>
2024-11-18 17:38:54 +0900

    INSERT문 작성 중

--- src/main/java/com/mysite/restaurant/js/controller/TestController.java
+++ src/main/java/com/mysite/restaurant/js/controller/TestController.java
@@ -1,9 +1,18 @@
 package com.mysite.restaurant.js.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class TestController {
 
+	@PostMapping("/test")
+	public String posttest(@RequestBody String entity) {
+		
+		
+		return entity;
+	}
 	
 }
