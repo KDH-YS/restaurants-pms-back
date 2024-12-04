b315ae438615dcb0472ceeb77461f44de391221a
Jammomu <pridom03@naver.com>
2024-12-04 23:01:15 +0900

    코드 정리

--- src/main/java/com/mysite/restaurant/hj/controller/MailController.java
+++ src/main/java/com/mysite/restaurant/hj/controller/MailController.java
@@ -2,6 +2,7 @@ package com.mysite.restaurant.hj.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mysite.restaurant.hj.dto.EmailCheckDTO;
@@ -13,17 +14,18 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class MailController {
 
 	private final MailService mailService;
 	
-	@PostMapping ("/mailSend")
+	@PostMapping ("/verify-email")
     public String mailSend(@RequestBody @Valid EmailRequestDTO emailDto) {
         System.out.println("이메일 인증 이메일 :" + emailDto.getEmail());
         return mailService.joinEmail(emailDto.getEmail());
     }
 	
-	@PostMapping("/mailCheck")
+	@PostMapping("/verify-emailCheck")
     public String AuthCheck(@RequestBody @Valid EmailCheckDTO emailCheckDTO) {
         Boolean Checked=mailService.CheckAuthNum(emailCheckDTO.getEmail(), emailCheckDTO.getAuthNum());
         if (Checked) {
--- src/main/java/com/mysite/restaurant/hj/service/MailService.java
+++ src/main/java/com/mysite/restaurant/hj/service/MailService.java
@@ -50,7 +50,7 @@ public class MailService {
 		makeRandomNumber();
 		String setFrom = "dionisos198@naver.com"; // email-config에 설정한 자신의 이메일 주소를 입력
 		String toMail = email;
-		String title = "회원 가입 인증 이메일 입니다."; // 이메일 제목
+		String title = "레슐랭코리아 회원가입 인증 이메일 입니다."; // 이메일 제목
 		String content =
 		        "레슐랭코리아를 방문해주셔서 감사합니다." + // html 형식으로 작성!
 			    "<br><br>" +
