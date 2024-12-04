14efba70e3b9745b6e8309dc5d25e124b9fac292
joo2296 <joo2296@gmail.com>
2024-12-04 16:45:11 +0900

    가게리뷰에서 신고구현

--- src/main/java/com/mysite/restaurant/js/model/ReportReason.java
+++ src/main/java/com/mysite/restaurant/js/model/ReportReason.java
@@ -1,5 +1,21 @@
 package com.mysite.restaurant.js.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ReportReason {
-	INAPPROPRIATE, FAKE, OFFENSIVE, OTHER
+	FAKE("인증되지 않은 글"),
+	INAPPROPRIATE("부적절한 내용"),
+	OFFENSIVE("모욕적"),
+	OTHER("기타");
+
+	private final String korean;
+
+	ReportReason(String korean) {
+		this.korean = korean;
+	}
+
+	@JsonValue
+	public String getKorean() {
+		return korean;
+	}
 }
