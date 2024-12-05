d4cd0daf6453d389636d4be8dca7b81da9aecbe7
kdh <gloian@naver.com>
2024-12-05 15:57:26 +0900

    병합 오류 수정

--- src/main/java/com/mysite/restaurant/WebConfig.java
+++ src/main/java/com/mysite/restaurant/WebConfig.java
@@ -14,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 인증 정보 허용
+        // 추가적인 경로에 대해 CORS 허용
+        registry.addMapping("/payment/**")  // /payment/** 경로에 대해 CORS 허용
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(false);  // 인증된 요청을 허용
     }
 }
