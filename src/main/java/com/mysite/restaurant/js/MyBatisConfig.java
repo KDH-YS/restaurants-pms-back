176111f3c9b3cee4e994f5a2ba9814f9a805486c
joo2296 <joo2296@gmail.com>
2024-12-05 14:55:38 +0900

    통합 ( webConfig에 맵퍼 매칭 )

--- src/main/java/com/mysite/restaurant/js/MyBatisConfig.java
+++ src/main/java/com/mysite/restaurant/js/MyBatisConfig.java
@@ -1,9 +1,13 @@
-package com.mysite.restaurant.js;
+package com.mysite.restaurant;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages = "com.mysite.restaurant.js.mapper")
+@MapperScan(basePackages = {
+        "com.mysite.restaurant.hj.mapper",
+        "com.mysite.restaurant.jh",
+        "com.mysite.restaurant.kdh.Mappers",
+        "com.mysite.restaurant.js.mapper"})
 public class MyBatisConfig {
 }
